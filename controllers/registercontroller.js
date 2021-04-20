fdf08220b690f8193f6cce4dc2e2e12d7f71f973
bodeby <thorbensen@gmail.com>
2021-04-20 14:58:03 +0200

    Update on html

--- controllers/registercontroller.js
+++ controllers/registercontroller.js
@@ -6,7 +6,10 @@ let UserProfile = require("../models/user_profile");
 let saltRounds = 10;
 
 exports.register_get = function (req, res, next) {
-  res.render("register", { title: "Register", messages: null });
+  res.render("register", {
+    title: "Register",
+    route: "/register",
+  });
 };
 
 exports.register_post = [
