df6021e20c511f441bf9b6780cc718439ca72b2f
Vindemoeller <72599564+Vindemoeller@users.noreply.github.com>
2021-04-23 11:02:00 +0200

    device and user_profile energy update functions

---
+++ scripts/device_energy_consumption.js
@@ -0,0 +1,3 @@
+// loop over all devices with state: "ON"
+// update the last day's energy use array
+// update the device's total energy use
---
+++ scripts/user_profile_energy_consumption.js
@@ -0,0 +1,3 @@
+// loop over all/a user profiles update their last day's energy use array -
+// - by looping over all of their devices -> user profile needs a device list
+// update each user profile's total energy use
