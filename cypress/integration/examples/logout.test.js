4c9c20e7b02145b3e23a158831e7efb81346de82
Mihusu <ming2200@hotmail.com>
2021-04-29 15:22:37 +0200

    Logout and settings test are done and device test is almost done

---
+++ cypress/integration/examples/logout.test.js
@@ -0,0 +1,25 @@
+describe('My logout Test', () => {
+    it('Visits the login page for any user', () => {
+        cy.visit('localhost:3000/login');
+        cy.get('.action-register').click();
+        cy.get('.action-login').click();
+
+        // Get an email input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com');
+        
+        // Get an password input, type into it and verify that the value has been updated
+        cy.get('.action-password')
+        .type('Ming#1359')
+        .should('have.value', 'Ming#1359');
+        cy.get('.action-submit').click();
+        
+        // When you login a test will be conducted to check if the user got a cookie.
+        cy.getCookie('auth').should('have.property','value','608911a0070ea135f09e3705')
+        cy.wait(6000);
+
+        cy.get('.action-menu').click();
+        cy.get('.logout-button').click();
+    });
+});
