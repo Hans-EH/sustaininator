78f55c74c6a8ba14c7ccdc038c55011fc1e2fa91
jakobsaadbye <31799001+jakobsaadbye@users.noreply.github.com>
2021-05-03 16:08:07 +0200

    merge

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,122 +1,294 @@
 // auth controller for validating that user is logged in
 let auth = require("../controllers/AuthController");
-
+let graph_data = require("../models/cache_graph_data");
 // Node fetch package
 const fetch = require("node-fetch");
 
+
+//datapoints on 1 day
+const DATADAY = (60 * 24) / 5;
+
 // Controller for calculating CO2 emission data
 exports.co2emission = function (req, res, next) {
-  async function fetchCO2energidata() {
+  async function fetchCO2data() {
     try {
       // Virker
-      const DAYSAMOUNT = 30; //change in URI if this is changed.
       const URI =
         'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT"Minutes5UTC", "Minutes5DK", "PriceArea", "CO2Emission" FROM "co2emis" ORDER BY "Minutes5UTC" DESC LIMIT 17280'; //every second is a correct datapoint, we need for a month of datapoints which is 17280
       let data = await fetch(URI).then((response) => response.json());
 
-      // Arrays for labels and values
-      let dataLabels = [];
       let dataValues = [];
-
-      let daysMonth = 30;
-      let daysWeek = 7;
-      let daysDays = 3;
-
-      let dataValuesMonth = [];
-      let dataValuesWeek = [];
-      let dataValuesDays = [];
-
-      //datapoints on 1 day
-      let dataDay = (60 * 24) / 5;
-      // Iterate over reponse results
-      console.log("Total CO2 Rows:" + data.result.records.length);
 
       for (let i = 0; i < data.result.records.length; i++) {
         // Push labels and values
         if (data.result.records[i].PriceArea == "DK1") {
           dataValues.push(data.result.records[i].CO2Emission);
-          dataLabels.push(data.result.records[i].Minutes5DK.slice(-8, -3));
         }
       }
       // Reverse Arrays
-      dataLabels = dataLabels.reverse();
       dataValues = dataValues.reverse();
 
-      //Function that creates a moving average dataset depended on amount of days.
-      function createData(days, newDataValues) {
-        //total data points
-        let dataTotal = days * dataDay;
-        for (let j = 0; j < dataDay; j++) {
-          newDataValues[j] = 0;
-          for (let i = j; i < dataTotal; i += dataDay) {
-            //compounds the values
-            newDataValues[j] +=
-              dataValues[i + (dataDay * DAYSAMOUNT - dataTotal)];
-          }
-          //finds average for that time
-          newDataValues[j] = newDataValues[j] / days;
-        }
-      }
+      //saving data to mongodb
+      //var new_graph_data = new graph_data({
+      //  date: Date(),
+      //  carbon_30: dataValuesMonth,
+      //  carbon_7: dataValuesWeek,
+      //  carbon_3: dataValuesDays,
+      //  carbon_1: dataValues,
+      //  labels_1: dataLabels,
+      //})
+      //new_graph_data.save(function (err, result) {
+      //  if (err) {
+      //    console.log(err);
+      //  }
+      //  else {
+      //    //console.log(result)
+      //  }
+      //})
 
-      createData(daysMonth, dataValuesMonth);
-      createData(daysWeek, dataValuesWeek);
-      createData(daysDays, dataValuesDays);
-
-      console.log("DataValues length" + dataValues.length + "\n");
-      console.log("DataValuesMonth length" + dataValuesMonth.length + "\n");
-      console.log("DataLabels length" + dataLabels.length + "\n");
-
-      //a function that creates the percentile line using preexisting moving average and a percentile.
-      function createPercentileLine(p, inputData) {
-        //percentile to calculate to.
-        //p = Math.ceil(100 / p);
-        inputData.sort(compareNumbers);
-        //calculating percentile
-        let percentileNr = Math.floor((inputData.length / 100) * p);
-        //new dataline to be filled for use as a graph.
-        let newDataLine = [];
-        console.log(
-          100 -
-            p +
-            "% saving, nr: " +
-            percentileNr +
-            ", value: " +
-            inputData[percentileNr]
-        );
-        //duplicate the result to every datapoint through the day
-        //in order for it to be plottet nicely
-        for (let i = 0; i < dataDay; i++) {
-          newDataLine[i] = inputData[percentileNr];
-        }
-        return newDataLine;
-      }
-
-      //comparenumbers function, to be used with sort.
-      function compareNumbers(a, b) {
-        return a - b;
-      }
-
-      res.json(data);
+      res.json(dataValues);
     } catch (error) {
       console.error(error);
       res.send(false);
     }
   }
 
-  fetchCO2energidata();
+  fetchCO2data();
 };
 
-exports.tester = function (req, res, next) {
-  async function testFetch() {
+// Controller for fetching and posting label data.
+exports.co2emissionlabels = function (req, res, next) {
+  async function fetchlabeldata() {
     try {
-      const URI = process.env.WEB_HOST + "data/co2emission";
+      // Virker
+      const URI =
+        'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT"Minutes5UTC", "Minutes5DK", "PriceArea", "CO2Emission" FROM "co2emis" ORDER BY "Minutes5UTC" DESC LIMIT 17280'; //every second is a correct datapoint, we need for a month of datapoints which is 17280
       let data = await fetch(URI).then((response) => response.json());
 
-      res.json(data);
+      let dataLabels = [];
+
+      for (let i = 0; i < data.result.records.length; i++) {
+        // Push labels
+        if (data.result.records[i].PriceArea == "DK1") {
+          dataLabels.push(data.result.records[i].Minutes5DK.slice(-8, -3));
+        }
+      }
+      // Reverse Arrays
+      dataLabels = dataLabels.reverse();
+
+      res.json(dataLabels);
+    } catch (error) {
+      console.error(error);
+      res.send(false);
+    }
+  }
+
+  fetchlabeldata();
+};
+
+//Function that creates a moving average dataset depended on amount of days.
+function createData(days, dataValues, newDataValues) {
+  //total data points
+  let dataTotal = days * DATADAY;
+  for (let j = 0; j < DATADAY; j++) {
+    newDataValues[j] = 0;
+    for (let i = j; i < dataTotal; i += DATADAY) {
+      //compounds the values
+      newDataValues[j] +=
+        dataValues[i + (DATADAY * 30 - dataTotal)];
+    }
+    //finds average for that time
+    newDataValues[j] = newDataValues[j] / days;
+  }
+}
+
+//exports moving average for co2 emissions throughout 30 days
+exports.carbon_30 = function (req, res, next) {
+  async function carbon_30_post() {
+    try {
+      let carbon_30 = [];
+      const URI = process.env.WEB_HOST + "data/co2emission";
+      let data = await fetch(URI).then((response) => response.json());
+      createData(30,data,carbon_30);
+      res.json(carbon_30);
     } catch (error) {
       console.log(error);
     }
   }
+  carbon_30_post();
+};
 
-  testFetch();
+//exports moving average for co2 emissions throughout 7 days
+exports.carbon_7 = function (req, res, next) {
+  async function carbon_7_post() {
+    try {
+      let carbon_7 = [];
+      const URI = process.env.WEB_HOST + "data/co2emission";
+      let data = await fetch(URI).then((response) => response.json());
+      createData(7,data,carbon_7);
+      res.json(carbon_7);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  carbon_7_post();
+};
+
+//exports moving average for co2 emissions throughout 3 days
+exports.carbon_3 = function (req, res, next) {
+  async function carbon_3_post() {
+    try {
+      let carbon_3 = [];
+      const URI = process.env.WEB_HOST + "data/co2emission";
+      let data = await fetch(URI).then((response) => response.json());
+      createData(3,data,carbon_3);
+      res.json(carbon_3);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  carbon_3_post();
+};
+
+
+//exports moving average for co2 emissions throughout 1 day
+exports.carbon_1 = function (req, res, next) {
+  async function carbon_1_post() {
+    try {
+      let carbon_1 = [];
+      const URI = process.env.WEB_HOST + "data/co2emission";
+      let data = await fetch(URI).then((response) => response.json());
+      createData(1,data,carbon_1);
+      res.json(carbon_1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  carbon_1_post();
+};
+
+
+
+// Controller for fetching onshore wind production
+exports.greenenergi_on_wind = function (req, res, next) {
+  async function fetch_on_wind_data() {
+    try {
+      const URI =
+        'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT "Minutes5DK", "PriceArea", "OffshoreWindPower", "OnshoreWindPower", "SolarPower" FROM "electricityprodex5minrealtime" ORDER BY "Minutes5UTC" DESC LIMIT 576';
+      let data = await fetch(URI).then((response) => response.json());
+      // Arrays for labels and values
+      let dataValuesOnWind = [];
+
+      // Iterate over reponse results
+      for (let i = 0; i < data.result.records.length; i++) {
+        // Push labels and values
+        if (data.result.records[i].PriceArea == "DK1") {
+          dataValuesOnWind.push(data.result.records[i].OnshoreWindPower);
+        }
+      }
+
+      // Reverse Arrays
+      dataValuesOnWind = dataValuesOnWind.reverse();
+
+      res.json(dataValuesOnWind);
+    } catch (error) {
+      console.error(error);
+      res.send(false);
+    }
+  }
+
+  fetch_on_wind_data();
+};
+
+// Controller for fetching offshore wind production
+exports.greenenergi_off_wind = function (req, res, next) {
+  async function fetch_off_wind_data() {
+    try {
+      const URI =
+        'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT "Minutes5DK", "PriceArea", "OffshoreWindPower", "OnshoreWindPower", "SolarPower" FROM "electricityprodex5minrealtime" ORDER BY "Minutes5UTC" DESC LIMIT 576';
+      let data = await fetch(URI).then((response) => response.json());
+      // Arrays for labels and values
+      let dataValuesOffWind = [];
+
+      // Iterate over reponse results
+      for (let i = 0; i < data.result.records.length; i++) {
+        // Push labels and values
+        if (data.result.records[i].PriceArea == "DK1") {
+          dataValuesOffWind.push(data.result.records[i].OffshoreWindPower);
+        }
+      }
+
+      // Reverse Arrays
+      dataValuesOffWind = dataValuesOffWind.reverse();
+
+      res.json(dataValuesOffWind);
+    } catch (error) {
+      console.error(error);
+      res.send(false);
+    }
+  }
+
+  fetch_off_wind_data();
+};
+
+// Controller for fetching solar production
+exports.greenenergi_solar = function (req, res, next) {
+  async function fetch_solar_data() {
+    try {
+      const URI =
+        'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT "Minutes5DK", "PriceArea", "OffshoreWindPower", "OnshoreWindPower", "SolarPower" FROM "electricityprodex5minrealtime" ORDER BY "Minutes5UTC" DESC LIMIT 576';
+      let data = await fetch(URI).then((response) => response.json());
+      // Arrays for labels and values
+      let dataValuesSolar = [];
+
+      // Iterate over reponse results
+      for (let i = 0; i < data.result.records.length; i++) {
+        // Push labels and values
+        if (data.result.records[i].PriceArea == "DK1") {
+          dataValuesSolar.push(data.result.records[i].SolarPower);
+        }
+      }
+
+      // Reverse Array
+      dataValuesSolar = dataValuesSolar.reverse();
+
+      res.json(dataValuesSolar);
+    } catch (error) {
+      console.error(error);
+      res.send(false);
+    }
+  }
+  fetch_solar_data();
+};
+
+
+// Controller for calculating CO2 emission data
+exports.greenenergi_labels = function (req, res, next) {
+  async function fetch_energi_labels() {
+    try {
+      const URI =
+        'https://www.energidataservice.dk/proxy/api/datastore_search_sql?sql=SELECT "Minutes5DK", "PriceArea", "OffshoreWindPower", "OnshoreWindPower", "SolarPower" FROM "electricityprodex5minrealtime" ORDER BY "Minutes5UTC" DESC LIMIT 576';
+      let data = await fetch(URI).then((response) => response.json());
+      // Arrays for labels and values
+      let data_labels = [];
+
+      // Iterate over reponse results
+      for (let i = 0; i < data.result.records.length; i++) {
+        // Push labels
+        if (data.result.records[i].PriceArea == "DK1") {
+          data_labels.push(data.result.records[i].Minutes5DK.slice(-8, -3)); 
+        }
+      }
+
+      // Reverse Arrays
+      data_labels = data_labels.reverse();
+
+      res.json(data_labels);
+    } catch (error) {
+      console.error(error);
+      res.send(false);
+    }
+  }
+
+  fetch_energi_labels();
 };
---
+++ cypress/integration/examples/settings.test.js
@@ -0,0 +1,58 @@
+/*
+Cypress is an end-to-end test where developers can set up, write, running and debugging tests,
+Cypress involves therefore testing an application’s workflow from beginning to end.
+*/
+describe('My settings page Test', () => {
+    it('Visits the login page for any user', () => {
+        cy.visit('localhost:3000/login');
+        cy.get('.action-register').click();
+        cy.get('.action-login').click();
+
+        // Get an email input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com');
+        
+        // Get an password input, type into it and verify that the value has been updated
+        cy.get('.action-password')
+        .type('Ming#1359')
+        .should('have.value', 'Ming#1359');
+        cy.get('.action-submit').click();
+        
+        // When you login a test will be conducted to check if the user got a cookie.
+        cy.getCookie('auth').should('have.property','value','608911a0070ea135f09e3705')
+
+        // Enters the settings page and then enters new values
+        cy.get('.action-menu').click();
+        cy.get('.settings-button').click();
+
+        cy.get('.first-name-text').click()
+        .clear()
+        .type('Fake')
+        .should('have.value', 'Fake');
+
+        cy.get('.last-name-text').click()
+        .clear()
+        .type('Fakesen')
+        .should('have.value', 'Fakesen');
+
+        // Invoke a function on the previously yielded subject, then use that to have equal to the subject.
+        cy.get('select').select(['EUR'])
+        .invoke('val')
+        .should('have.equal', 'EUR');
+        
+        cy.get('.update-user-settings').click();
+
+        cy.get('.action-menu').click();
+        cy.get('.settings-button').click();
+
+        cy.get('.sustainability-choice')
+        .clear()
+        .type('46')
+        .should('have.value', '46');
+
+        cy.get('.update-sustainability-choice').click();
+
+        cy.get('.home-button').click();
+    });
+});
