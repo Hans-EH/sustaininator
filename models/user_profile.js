a5576e63864dcbba3700d6b9c7729946cfaf1e14
bodeby <thorbensen@gmail.com>
2021-05-17 13:21:29 +0200

    update user

--- models/user_profile.js
+++ models/user_profile.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 // User Profile Model
 let userProfileSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  created: { type: Date, default: Date.now }
   devices: [{ type: Schema.Types.ObjectId, ref: "Device" }],
   advices: [{ type: Schema.Types.ObjectId, ref: "AdviceCard" }],
   firstname: { type: String },
@@ -14,7 +15,7 @@ let userProfileSchema = new Schema({
   carbon_footprint: { type: Number },
   sustainable_goals: { type: Number },
   total_energy_consumption_last_day: { type: Array, default: new Array(288).fill(0) },
-  carbon_score_last_day: { type: [Boolean], default: new Array(288).fill(false)},
+  carbon_score_last_day: { type: [Boolean], default: new Array(288).fill(false) },
 });
 
 module.exports = mongoose.model("UserProfile", userProfileSchema);
